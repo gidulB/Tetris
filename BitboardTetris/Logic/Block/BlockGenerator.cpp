9f1910b47366f4a002186f8571cd001431e9caa0
gidulB <rilokkuma@naver.com>
2025-06-25 20:50:13 +0900

    BlockGenerator 생성 중

---
+++ BitboardTetris/Logic/Block/BlockGenerator.cpp
@@ -0,0 +1,67 @@
+#include "BlockGenerator.h"
+#include "Block.h"
+
+int BlockGenerator::GenerateBlock(int blockType, int rotation)
+{
+	return 0;
+}
+
+int BlockGenerator::GenerateBlock(int blockType, int rotation, int color, int shape, int size, int position, int orientation)
+{
+	return 0;
+}
+
+int BlockGenerator::GetBlockType(int block)
+{
+	return 0;
+}
+
+int BlockGenerator::GetBlockRotation(int block)
+{
+	return 0;
+}
+
+int BlockGenerator::GetBlockColor(int block)
+{
+	return 0;
+}
+
+int BlockGenerator::GetBlockShape(int block)
+{
+	return 0;
+}
+
+int BlockGenerator::GetBlockSize(int block)
+{
+	return 0;
+}
+
+int BlockGenerator::GetBlockPosition(int block)
+{
+	return 0;
+}
+
+int BlockGenerator::GetBlockOrientation(int block)
+{
+	return 0;
+}
+
+int BlockGenerator::GenerateBlockInternal(int blockType, int rotation, int color, int shape, int size, int position, int orientation)
+{
+	return 0;
+}
+
+int BlockGenerator::ExtractBlockProperty(int block, int propertyMask, int propertyShift)
+{
+	return 0;
+}
+
+int BlockGenerator::CreateBlock(int blockType, int rotation, int color, int shape, int size, int position, int orientation)
+{
+	return 0;
+}
+
+int BlockGenerator::CombineBlockProperties(int blockType, int rotation, int color, int shape, int size, int position, int orientation)
+{
+	return 0;
+}
